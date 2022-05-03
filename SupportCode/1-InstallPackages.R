cd0b9a0217759ac0fc8d6a7438541f4f447219cb
Sara Chronister <57767797+sara-chronister@users.noreply.github.com>
2022-05-03 11:53:56 -0700

    Update 1-InstallPackages.R
    Update to list and only installs (no loading in this step)

--- SupportCode/1-InstallPackages.R
+++ SupportCode/1-InstallPackages.R
@@ -1,6 +1,37 @@
 # checks that required packages are installed, installs them if not, loads all required packages
 
-packages <- c("tidyverse","MMWRweek","jsonlite","httr","plotly","eulerr","reactable","keyring","ggraph","igraph","plyr","widyr","tidytext","splitstackshape","readxl","lubridate","dplyr")
+packages <- c("magrittr",
+              "RCurl",
+              "rmarkdown",
+              "flexdashboard",
+              "MMWRweek",
+              "jsonlite",
+              "httr",
+              "treemap",
+              "leaflet",
+              "devtools",
+              "haven",
+              "plotly",
+              "rpivotTable",
+              "eulerr",
+              "reactable",
+              "DT",
+              "keyring",
+              "ggraph",
+              "igraph",
+              "plyr",
+              "widyr",
+              "tidytext",
+              "splitstackshape",
+              "xtable",
+              "openxlsx",
+              "janitor",
+              "lubridate",
+              "dplyr",
+              "readr",
+              "jpeg",
+              "d3treeR",
+              "Rnssp")
 
 # Install packages not yet installed
 installed_packages <- packages %in% rownames(installed.packages())
@@ -8,8 +39,14 @@ if (any(installed_packages == FALSE)) {
   install.packages(packages[!installed_packages])
 }
 
+# packages not on CRAN
+if ("Rnssp" %in% rownames(installed.packages()) == FALSE) {
+  devtools::install_github("cdcgov/Rnssp")
+}
+
+if ("d3treeR" %in% rownames(installed.packages()) == FALSE) {
+  devtools::install_github("timelyportfolio/d3treeR")
+}
+
 # Packages loading
-invisible(lapply(packages, library, character.only = TRUE))
-
-
-# source: https://statsandr.com/blog/an-efficient-way-to-install-and-load-r-packages/
+# invisible(sapply(packages, library, character.only = TRUE))

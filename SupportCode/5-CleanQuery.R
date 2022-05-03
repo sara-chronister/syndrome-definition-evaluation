73d6ee3c9054317d864b6bd9928d427b0d77e399
Sara Chronister <57767797+sara-chronister@users.noreply.github.com>
2022-05-03 11:57:09 -0700

    Update and rename 4-CleanQuery.R to 5-CleanQuery.R

--- SupportCode/5-CleanQuery.R
+++ SupportCode/5-CleanQuery.R
@@ -5,15 +5,18 @@ clean_query_essence <- function(query) {
   clean_query <- data.frame(Element = query) %>%
     mutate(Element = str_to_lower(Element)) %>%
     mutate(Element = str_replace_all(Element, ",andnot,\\^.*?\\^|,andnot,\\(.*?\\)|\\|", "")) %>%
+    # mutate(Element = ifelse(str_detect(Element, "[[:alnum:]]\\^[[:alnum:]]"), str_replace(Element, "")))
+    mutate(Element = str_replace_all(Element, "(?<=[[:alnum:]])\\^(?=[[:alnum:]])", ",or,")) %>%
     mutate(Element = str_replace_all(Element, "!","^")) %>%
     mutate(Element = str_replace_all(Element,",and,",",or,")) %>%
     mutate(Element = str_replace_all(Element,",or,","|")) %>%
+    mutate(Element = str_replace_all(Element, "_", "[.]")) %>%
     cSplit(., splitCols = "Element", sep = "|", type.convert = FALSE) %>%
     pivot_longer(cols = starts_with("Element"), values_to = "Element") %>%
     mutate(Element = str_replace_all(Element,"\\[;/ \\]|\\[;/\\]","")) %>%
-    mutate(Element = str_replace_all(Element,"\\)|\\(|\\^|,|;|/|\\.","")) %>%
+    mutate(Element = str_replace_all(Element,"\\)|\\(|\\^|,|;|/|(?<!\\[)\\.","")) %>%
     mutate(Type = case_when(
-      str_detect(Element,"v[[:digit:]]") ~ "CCDD Category (see ESSENCE)",
+      str_detect(Element," v[[:digit:]]") ~ "CCDD Category (see ESSENCE)",
       str_detect(Element,"[[:digit:]]") ~ "Diagnosis Code",
       str_detect(Element, "[[:digit:]]", negate = TRUE) ~ "Syndrome Term")) %>%
     select(-name, `Syndrome Element` = Element, `Element Type` = Type) %>%
@@ -44,7 +47,8 @@ detect_elements <- function(data, terms, text_field) {
   }
   
   terms_detected <- purrr::reduce(terms_detected_list,full_join) %>%
-    select(EssenceID,TruePositive,text_field,everything())
+    select(EssenceID,TruePositive,text_field,everything()) %>%
+    distinct()
   return(terms_detected)  
   
 }

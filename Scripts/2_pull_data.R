4ef5828ea3f60031a77b4f0f2f3716c6b3823b89
Tyler Bonnell <100374471+DOH-TJB0303@users.noreply.github.com>
2024-03-19 17:33:41 -0700

    Finish pulling data, identifying overlap, storing data in lists, and writing data as .csv and .RData

--- Scripts/2_pull_data.R
+++ Scripts/2_pull_data.R
@@ -7,60 +7,9 @@
 
 # =======================================================================
 
-# Define Analysis Parameters -----
+# Pull & Clean Data -----
 
-## Define params list
-params <- list()
-
-## Define Parameters
-
-### Number of Queries to Evaluate
-params$n_queries_eval <- n_queries_eval
-
-#### Output Folder
-params$output_folder <- case_when(
-  params$n_queries_eval == 1 ~ "Output/OneDef/",
-  params$n_queries_eval == 2 ~ "Output/TwoDefs/",
-  params$n_queries_eval == 3 ~ "Output/ThreeDefs/")
-
-fs::dir_create(params$output_folder) # Create Folder
-
-### Dates & Geographies
-params$start_date <- DefinitionInformation[["Setup"]]$StartDate %>% as.Date()
-params$end_date <- DefinitionInformation[["Setup"]]$EndDate %>% as.Date()
-params$jurisdiction <- DefinitionInformation[["Setup"]]$Jurisdiction
-
-### ESSENCE Fields to Pull & Analyze
-
-#### All Fields to Pull in API
-params$fields <-  DefinitionInformation[["SelectFields"]] %>%
-  mutate(includeAPI = ifelse(IncludeForValidationReview =="Yes"| IncludeForTextAnalysis =="Yes", "Yes", "No")) %>%
-  filter(includeAPI == "Yes")
-
-#### All Fields (as API URL)
-params$fields_api_url <- params$fields %>%
-  pull(ESSENCEfields) %>%
-  paste0(., collapse = "&field=") %>%
-  paste0("&field=",.)
-
-#### Text Mining
-params$fields_text_analysis <- params$fields %>%
-  filter(IncludeForTextAnalysis =="Yes" & (is.na(Notes)| str_detect(Notes, "Not eligible", negate=TRUE))) %>% # Some fields are not eligible for text mining (see Notes)
-  pull(ESSENCEfields)
-
-#### Validation Review (Line-Level Records)
-params$fields_validation_review <- params$fields %>%  
-  filter(IncludeForValidationReview =="Yes") %>%
-  pull(ESSENCEfields)
-
-### Data Cleaning
-params$deduplicate_ddx <- DefinitionInformation[["ValidationReviewInformation"]] %>% pull_no_na(df=., variable = "DeduplicateDDx")
-
-
-
-# Pull Data -----
 ## Description: Create a list for each definition evaluated
-
 for(i in 1:params$n_queries_eval){
   
   ### Create Definition-Specific Storage List
@@ -92,7 +41,7 @@ for(i in 1:params$n_queries_eval){
   ## Results -----
   
   ### Pull API Data (DataDetails)
-  defX_list$results$raw <- get_longterm_details(url = defX_list$setup$url, 
+  defX_list$results$raw <- get_essence_data(url = defX_list$setup$url, 
                                                 start_date = params$start_date, end_date = params$end_date)
   ### Clean API Data (DataDetails) 
   defX_list$results$clean <- defX_list$results$raw %>%
@@ -124,25 +73,128 @@ for(i in 1:params$n_queries_eval){
 }
 
 
-# Compare Data -----
+# Assess Definition Overlap -----
+
+## Full Join Data
+if(params$n_queries_eval == 2){
+  
+  Overlap <- full_join(def1_list$results$clean, def2_list$results$clean) 
+  
+}else if(params$n_queries_eval == 3){
+  
+  Overlap <- full_join(def1_list$results$clean, def2_list$results$clean) %>%
+    full_join(., def3_list$results$clean)
+}
+
+## Convert NAs to 0's and Sum DefX Categories
+Overlap <- Overlap %>%
+  mutate(across(
+    .cols = starts_with("def", ignore.case = FALSE),
+    .fns = ~ifelse(is.na(.), 0, .))) %>%
+  mutate(Total_Defs = rowSums(select(., starts_with("def", ignore.case = FALSE))))# add all defX columns together
+
+## Rename Def# Variables to Query Abbreviations
+if(params$n_queries_eval == 2){
+  
+  Overlap <- Overlap %>%
+    rename(!!params$queries_abbrev[1] := def1,
+           !!params$queries_abbrev[2] := def2)
+  
+}else if(params$n_queries_eval == 3){
+  
+  Overlap <- Overlap %>%
+    rename(!!params$queries_abbrev[1] := def1,
+           !!params$queries_abbrev[2] := def2,
+           !!params$queries_abbrev[3] := def3)
+}
+  
+## Create Combinations Variable to Categorize Overlap
+Overlap$Definitions <- apply(Overlap[,params$queries_abbrev], 
+                              MARGIN = 1, 
+                              function(data) paste(names(which(data == 1)),collapse = ", "))
+         
+
+# Store Data -----
+syndrome_eval_list <- list()
 
 
+## Store Individual Queries within syndrome_eval_list
+for(i in 1:params$n_queries_eval){
+  syndrome_eval_list[[i]] <- get(paste0("def",i,"_list"))
+}
 
+names(syndrome_eval_list) <- params$queries_abbrev
 
+## Add Overlap DF
+if(params$n_queries_eval > 1){
+  
+  syndrome_eval_list$Overlap <- Overlap %>%
+    select(Date, C_BioSense_ID, all_of(params$queries_abbrev), Total_Defs, Definitions, everything()) # Reorder columns
+}
+
+## Add Row Totals (via Overlap DF)
+syndrome_eval_list$defs_total <- nrow(syndrome_eval_list$Overlap)
+syndrome_eval_list$defs_total_pretty <- format(syndrome_eval_list$defs_total, big.mark = ",", scientific = FALSE)
 
 
 
 # Save Data -----
 
 ## .RData Files
-if(params$n_queries_eval == 1){
-  save(params, def1_list, file = paste0(params$output_folder,"Data.RData")) 
-  
-}else if(params$n_queries_eval == 2){
-  save(params, def1_list, def2_list, file = paste0(params$output_folder,"Data.RData")) 
-  
-}else if(params$n_queries_eval == 3){
-  save(params, def1_list, def2_list, def3_list, file = paste0(params$output_folder,"Data.RData")) 
-}
+save(params, syndrome_eval_list, file = paste0(params$filepaths$output,"Data.RData"))
+
 
 ## .csv Files
+
+# Match Only 1 Definition
+if(params$n_queries_eval > 1){ # If only 1 definition is evaluated use "All Defs Visits.csv"
+  for(i in 1:params$n_queries_eval){
+    
+    syndrome_eval_list$Overlap %>%
+      filter(Definitions == params$queries_abbrev[i]) %>%
+      write.csv(file = paste0(params$filepaths$definition_overlap,"1_", params$queries_abbrev[i], " Only Visits.csv"))
+  }
+}
+
+
+## Match Only 2 Definitions
+if(params$n_queries_eval == 3){ # 2 definition combinations are only of interest if evaluating 3 definitions simultaneously. 1 definition being evaluated (combinations not possible); 2 definitions being evaluated used "All Defs Visits.csv"
+  
+  ### Identify All Unique 2 Definition Combinations
+  combinations <- syndrome_eval_list$Overlap %>% 
+    filter(Total_Defs == 2) %>% 
+    distinct(Definitions) %>%
+    pull(Definitions)
+  
+  ### Create 2 Definition Combination File Names
+  combination_filenames <- combinations %>%
+    str_to_upper(.) %>%
+    str_replace_all(string = ., pattern = ",", replacement = " and")
+  
+  ### For Loop: Filter & Write CSV for all Unique Combinations
+  for(i in seq_along(combinations)){
+    
+    syndrome_eval_list$Overlap %>%
+      filter(Definitions == combinations[i]) %>%
+      write.csv(file = paste0(params$filepaths$definition_overlap,"2_", combination_filenames[i], " Visits.csv"))
+  }
+}
+
+
+## Match All Definitions
+all_matched_filename <- syndrome_eval_list$Overlap %>% 
+  filter(Total_Defs == params$n_queries_eval) %>% 
+  distinct(Definitions) %>%
+  pull(Definitions)%>%
+  str_to_upper(.) %>%
+  str_replace_all(string = ., pattern = ",", replacement = " and")
+
+### Write CSV
+syndrome_eval_list$Overlap %>%
+  filter(Total_Defs == params$n_queries_eval) %>%
+  write.csv(file = paste0(params$filepaths$definition_overlap,params$n_queries_eval,"_", all_matched_filename, " Visits.csv"))
+
+
+# All Visits (Regardless of Matching/Overlap)
+syndrome_eval_list$Overlap %>%
+  write.csv(file = paste0(params$filepaths$definition_overlap, "All Visits.csv"))

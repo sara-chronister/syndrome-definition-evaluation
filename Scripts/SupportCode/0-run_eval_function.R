fc16dc53c4af90771cd5dea827440e622cf38589
Tyler Bonnell <Tyler.Bonnell@co.snohomish.wa.us>
2024-10-02 17:06:20 -0700

    Update documentation

--- Scripts/SupportCode/0-run_eval_function.R
+++ Scripts/SupportCode/0-run_eval_function.R
@@ -13,13 +13,16 @@ run_evaluation <- function(n_defs_eval,
   DefinitionInformation <- multiplesheets(fname = def_info_table)
   
   ## 1a. Return the User Selected Options to the Global Environment
-  ### Description: This is needed to execute the source() scripts.
+  ### Description: This is needed in the Global Environment to render the .Rmd template.
   assign("DefinitionInformation", DefinitionInformation, envir = .GlobalEnv)
   
   
   # Step 2: Perform Checks to Ensure Tool is Being Utilized Correctly.
   if(n_defs_eval > 3){stop("This tool can only analyze up to 3 syndromes simultaneously. Please change n_defs_eval to a value <= 3.")}
 
+  ## 2a. Return n_defs_eval to the Global Environment
+  ### Description: This is needed in the Global Environment to render the .Rmd template.
+  assign("n_defs_eval", n_defs_eval, envir = .GlobalEnv)
   
   # Step 3: Source Pre-Processing Scripts
   
